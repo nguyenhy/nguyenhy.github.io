173fe8ff53dfe173c1309030040e9ec02449b30d
nguyenhy <nguyenhy311@gmail.com>
2022-01-06 22:59:08 +0700

    feat(home): init home layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="drawer">
+      <div class="overlay"></div>
+      <div class="wrap">
+        <div class="logo">
+          <router-link to="/">
+            <span class="is-size-1">hyn</span>
+          </router-link>
+        </div>
+        <div class="slogan">
+          <span class="is-size-5"
+            >Keep your eyes on the skies, you will rise to the glory of who you
+            are.</span
+          >
+        </div>
+        <nav class="nav">
+          <ul>
+            <li>
+              <router-link class="hyn-highlight" to="/blog">Blog</router-link>
+            </li>
+            <li>
+              <router-link class="hyn-highlight" to="/showcase"
+                >Showcase</router-link
+              >
+            </li>
+            <li>
+              <router-link class="hyn-highlight" to="/resume"
+                >Resumé</router-link
+              >
+            </li>
+          </ul>
+        </nav>
+        <nav class="social">
+          <ul>
+            <li class="hyn-highlight">
+              <a href="https://twitter.com/nguyenhy132">
+                <i class="hyni-twitter is-size-5"></i>
+              </a>
+            </li>
+            <li class="hyn-highlight">
+              <a href="https://github.com/nguyenhy">
+                <i class="hyni-github is-size-5"></i>
+              </a>
+            </li>
+            <li class="hyn-highlight">
+              <a href="mailto:nguyenhy311@gmail.com">
+                <i class="hyni-mail is-size-5"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss" src="./App.global.vue.scss"></style>
+<style lang="scss" scoped src="./App.vue.scss"></style>
