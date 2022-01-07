e5aa2d71b24187453cab380c828bf6fed0593276
nguyenhy <nguyenhy311@gmail.com>
2022-01-07 22:26:06 +0700

    feat(config): update eslint, npm script, vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,22 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const isProduction = process.env.NODE_ENV === 'production'
 
 const vueConfig = {
-    chainWebpack: config => {
-        config.externals({
-            'vue': 'Vue',
-            "vue-router": "VueRouter",
-        })
-    },
-    configureWebpack: {
-        plugins: [
-            new BundleAnalyzerPlugin()
-        ]
-    }
+  chainWebpack: config => {
+    config.externals({
+      'vue': 'Vue',
+      "vue-router": "VueRouter",
+    })
+  },
+  configureWebpack: {
+    plugins: [
+    ]
+  }
 }
+
+// vueConfig.configureWebpack.plugins.push(
+//   new BundleAnalyzerPlugin()
+// )
 
 
 module.exports = vueConfig
