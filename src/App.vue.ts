9d09526a2dda0e3e447ba0a8371bcc57a11ba0ca
nguyenhy <nguyenhy311@gmail.com>
2022-01-15 22:04:22 +0700

    feat(home): check navigate using browser buttons, shorcut keys

--- src/App.vue.ts
+++ src/App.vue.ts
@@ -1,4 +1,4 @@
-import { Component, ProvideReactive } from "vue-property-decorator";
+import { Component, ProvideReactive, Watch } from "vue-property-decorator";
 
 @Component({
   components: {
@@ -8,19 +8,29 @@ import { Component, ProvideReactive } from "vue-property-decorator";
 export default class App extends Vue {
   @ProvideReactive() showContent: boolean = false;
 
+  @Watch("$route.name")
+  onChangeRouteName(): void {
+    this.navigateAction(this.$route.name || "");
+  }
+
   public mounted(): void {
     console.log(this);
   }
 
   protected onNavigate(name: string): void {
+    this.navigateAction(name);
+  }
+
+  protected onClickToggleMenu(): void {
+    this.showContent = false;
+  }
+
+  private navigateAction(name: string) {
+    name = name.toLowerCase();
     if (name == "home") {
       this.showContent = false;
     } else {
       this.showContent = true;
     }
-  }
-
-  protected onClickToggleMenu(): void {
-    this.showContent = false;
   }
 }
