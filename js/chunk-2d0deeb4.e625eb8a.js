18487d1193736335cac744730c2a264b76a2ca9d
nguyenhy <nguyenhy311@gmail.com>
2022-01-23 15:17:27 +0700

    deploy

---
+++ js/chunk-2d0deeb4.e625eb8a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0deeb4"],{8864:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},c=[],l=t("2877"),r={},s=Object(l["a"])(r,u,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0deeb4.e625eb8a.js.map
