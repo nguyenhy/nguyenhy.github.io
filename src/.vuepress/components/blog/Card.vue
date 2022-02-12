e1a58fb18336609c733353e89f2168ef9d3336f2
nguyenhy <nguyenhy311@gmail.com>
2022-02-13 06:11:53 +0700

    feat(blog): update blog's card layout

--- src/.vuepress/components/blog/Card.vue
+++ src/.vuepress/components/blog/Card.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    <span>{{ item.title }}</span>
+  <div class="blog-item">
+    <div class="title">
+      <h4>
+        <a :href="item.href" :alt="item.title" class="has-text-weight-bold">{{
+          item.title
+        }}</a>
+      </h4>
+    </div>
+    <div class="meta"></div>
+    <div class="thumb-desc">
+      <div class="thumb">
+        <a :href="item.href" :alt="item.title">
+          <img
+            height="350"
+            width="800"
+            :src="item.thumb"
+            :alt="item.thumbAlt"
+          />
+        </a>
+      </div>
+      <div class="description has-background-grey-dark px-2">
+        <span class="has-text-grey-light">Description</span>
+        <p>{{ item.description }}</p>
+      </div>
+    </div>
+    <div class="summary has-background-grey-dark px-2">
+      <p class="is-italic">{{ item.summary }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,7 +34,7 @@
 <script>
 export default {
   props: {
-    item: Object
-  }
+    item: Object,
+  },
 };
 </script>
--- src/blog/README.md
+++ src/blog/README.md
@@ -2,9 +2,15 @@
 layout: Blog
 list:
 - title: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, nostrum?
+  description: Lorem ipsum dolor sit amet consectetur
   summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, sapiente molestiae inventore provident beatae corrupti. 
   thumb: https://res.cloudinary.com/hqe/image/upload/v1641481664/github-nguyenhy-hp/sky.jpg
+  thumbAlt: sky
+  href: /blog/2022/demo.html
 - title: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, nostrum?
+  description: Lorem ipsum dolor sit amet consectetur 
   summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, sapiente molestiae inventore provident beatae corrupti. 
   thumb: https://res.cloudinary.com/hqe/image/upload/v1641481664/github-nguyenhy-hp/sky.jpg
+  thumbAlt: sky
+  href: /blog/2022/demo.html
 ---
