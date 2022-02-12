0fa8e98e2c2c79a0e874c1fe3a0366a5400aeb20
nguyenhy <nguyenhy311@gmail.com>
2022-02-12 23:20:27 +0700

    feat(layout): setup header-main-footer layout

---
+++ src/.vuepress/components/layout/Layout.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="layout has-background-dark has-text-light">
+    <div class="wrap">
+      <header class="header p-2 mb-4">
+        <div class="level is-flex is-flex-direction-row">
+          <div class="level-left">
+            <a accesskey="h" class="logo" href="/">
+              <img
+                src="../../assets/logo-hyn_36x36.png"
+                width="36"
+                height="32"
+                alt="hyn's logo"
+              />
+              <span class="title has-text-weight-bold has-text-primary"
+                >'s {{ title }}</span
+              >
+            </a>
+          </div>
+          <div class="level-right m-0">
+            <button class="button has-background-dark has-text-light">
+              <span class="icon">
+                <i class="hyni-search"></i>
+              </span>
+            </button>
+          </div>
+        </div>
+      </header>
+      <main class="main">
+        <div class="container px-1">
+          <slot></slot>
+        </div>
+      </main>
+      <footer class="footer p-2 has-background-grey-dark">
+        <div class="content has-text-centered">
+          <p>
+            Power by
+            <strong><a class="has-text-primary" href="/resume">me</a></strong>
+          </p>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped src="./Layout.vue.scss"></style>
+<script>
+export default {
+  props: {
+    title: String,
+  },
+};
+</script>
