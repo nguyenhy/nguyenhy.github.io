9b9cf5bed9304e9dfd90299f67aa2d9621d71f46
nguyenhy <nguyenhy311@gmail.com>
2023-06-04 11:56:07 +0700

    feat(logo): use `hytorium_horizontal_long_trans` for logo

--- src/components/icons/qwik.tsx
+++ src/components/icons/qwik.tsx
@@ -1,3 +1,9 @@
 export const QwikLogo = () => {
-  return <img src="/assets/hytorium_trans.svg" width={60} height={60} />;
+  return (
+    <img
+      src="/assets/logo/hytorium_horizontal_long_trans.svg"
+      width={60 * 2.5}
+      height={24 * 2.5}
+    />
+  );
 };
