1186effd6a26dab22a4f5cbdf05979f5dbb8d438
nguyenhy <nguyenhy311@gmail.com>
2023-06-04 11:43:32 +0700

    feat(setup): add `tailwind`

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      logo: ['"Atomic\\ Age"', "cursive"],
+    },
+  },
+  plugins: [
+    {
+      "postcss-import": {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
