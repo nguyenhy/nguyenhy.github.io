d5b5a32b2d2d2bdc9d034ff2197e38b7018915e1
nguyenhy <nguyenhy311@gmail.com>
2023-06-05 14:08:28 +0700

    feat(logo): add `props` to load logo with expect `height`

--- src/components/icons/qwik.tsx
+++ src/components/icons/qwik.tsx
@@ -1,9 +1,19 @@
-export const QwikLogo = () => {
+import { component$ } from "@builder.io/qwik";
+
+interface ILogo {
+  /**
+   * expect height of logo
+   */
+  height?: number;
+}
+
+export default component$<ILogo>(({ height }) => {
+  const ratio = (height ?? 60) / 24;
   return (
     <img
       src="/assets/logo/hytorium_horizontal_long_trans.svg"
-      width={60 * 2.5}
-      height={24 * 2.5}
+      width={60 * ratio}
+      height={24 * ratio}
     />
   );
-};
+});
