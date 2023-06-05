e8bed8927b5f5e74574ef8960667561766a4c767
nguyenhy <nguyenhy311@gmail.com>
2023-06-05 14:09:56 +0700

    feat(header): update style: - use style from`tailwind` - add border - change nav route, name

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,35 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
-import { QwikLogo } from "../icons/qwik";
+import QwikLogo from "../icons/qwik";
 import styles from "./header.css?inline";
 
-export default component$(() => {
-  useStyles$(styles);
+interface IHeaderProps {}
 
+export default component$<IHeaderProps>(() => {
+  useStyles$(styles);
   const { url } = useLocation();
 
   return (
-    <header>
-      <a class="logo" href="/">
-        <QwikLogo />
-      </a>
-      <nav>
-        <a href="/docs" class={{ active: url.pathname.startsWith("/docs") }}>
-          Docs
+    <header class="sticky top-0 z-10 h-20 w-full py-2 bg-inherit overflow-hidden">
+      <div class="2xl:max-w-5xl container mx-auto flex flex-row justify-between items-center">
+        <a class="no-underline" href="/">
+          {/* h-20 ->  5rem: 80px */}
+          {/* p-2 -> 0.5rem: 8px */}
+          <QwikLogo height={80 - 8 * 2} />
         </a>
-        <a
-          href="/about-me"
-          class={{ active: url.pathname.startsWith("/about-me") }}
-        >
-          <span>About Me</span>
-        </a>
-      </nav>
+        <nav>
+          <a href="/blogs" class={{ active: url.pathname.startsWith("/docs") }}>
+            Blogs
+          </a>
+          <a
+            href="/about-me"
+            class={{ active: url.pathname.startsWith("/about-me") }}
+          >
+            <span>About Me</span>
+          </a>
+        </nav>
+      </div>
+      <div class="w-full mt-1 border-b border-solid border-b-black"></div>
     </header>
   );
 });
