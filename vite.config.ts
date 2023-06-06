8d087c25f60b6245962fbfadd53600e06fb8f802
nguyenhy <nguyenhy311@gmail.com>
2023-06-07 06:25:13 +0700

    feat: use `rehypePrettyCode` to format `mdx` code block

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,56 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+const rehypePrettyCodeOptions = {
+  // Use one of Shiki's packaged themes
+  theme: 'one-dark-pro',
+  // Or your own JSON theme
+  // Keep the background or use a custom background color?
+  keepBackground: true,
+ 
+  // Callback hooks to add custom logic to nodes when visiting
+  // them.
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{type: 'text', value: ' '}];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    // Each line node by default has `class="line"`.
+    node.properties.className.push('highlighted');
+  },
+  onVisitHighlightedWord(node) {
+    // Each word node has no className by default.
+    node.properties.className = ['word'];
+  },
+};
+
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity({
+        mdxPlugins: {
+          rehypeSyntaxHighlight: false,
+          remarkGfm: true,
+          rehypeAutolinkHeadings: true,
+        },
+        mdx: {
+          rehypePlugins: [
+            [
+              rehypePrettyCode,
+              rehypePrettyCodeOptions,
+            ],
+          ],
+        },
+      }),
+      qwikVite(),
+      tsconfigPaths(),
+    ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
