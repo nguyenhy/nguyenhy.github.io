c5ac861220026afdac65d17d900fa784a2375e3c
nguyenhy <nguyenhy311@gmail.com>
2023-06-07 06:29:40 +0700

    feat(layout): remove usage of `useContextProvider`

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,21 +1,8 @@
-import { useContextProvider, useOnDocument, useStore } from "@builder.io/qwik";
-import { $, component$, Slot } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
 import Footer from "~/components/footer/footer";
 import Header from "~/components/header/header";
-import type { ILayoutContextState } from "./layout.types";
-import { LayoutContext } from "./layout.const";
 
 export default component$(() => {
-  const layoutStore = useStore<ILayoutContextState>({ scrollTop: -1 });
-  useContextProvider(LayoutContext, layoutStore);
-
-  useOnDocument(
-    "scroll",
-    $(() => {
-      const scrollTop = document.scrollingElement?.scrollTop || 0;
-      layoutStore.scrollTop = scrollTop;
-    })
-  );
   return (
     <>
       <Header />
