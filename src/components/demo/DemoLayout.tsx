e8c29facad02025a285c666f2aa7d7db33f780df
nguyenhy <nguyenhy311@gmail.com>
2023-06-19 10:52:21 +0700

    feat(DemoLayout): only uppercase first word of demo title

--- src/components/demo/DemoLayout.tsx
+++ src/components/demo/DemoLayout.tsx
@@ -3,9 +3,11 @@ import type { IDemoLayoutOptions } from "./DemoLayout.types";
 export const DemoLayout = (props: IDemoLayoutOptions) => {
   return (
     <>
-      <div class="flex flex-col border-2 border-slate-700 dark:border-slate-200 rounded-md">
+      <div class="flex flex-col border-2 border-slate-700 dark:border-slate-200 rounded-md ">
         <div class="border-[inherit] border-b-2 p-2 flex flex-row justify-between">
-          <span class="text-xl font-bold capitalize">{props.title}</span>
+          <span class="text-xl font-bold first-letter:uppercase">
+            {props.title}
+          </span>
           {props.onReset == null ? null : (
             <button onClick$={props.onReset}>Reset</button>
           )}
