1ff7aff9ba852a76927dd1af313bb99d6ff53916
nguyenhy <nguyenhy311@gmail.com>
2023-06-26 16:19:12 +0700

    feat(chore): change arguments var name

--- src/services/options/index.ts
+++ src/services/options/index.ts
@@ -4,7 +4,7 @@
 export function getOptions<KS extends Record<string, any>, K extends keyof KS>(
   key: K,
   options: Partial<KS>,
-  defaultValue: KS
+  defaultOptions: KS
 ): KS[K] {
   if (key in options) {
     const value = (options as KS)[key];
