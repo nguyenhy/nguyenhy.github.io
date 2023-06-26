917633890f1bdfd0e91d72747c6f26e86ed9dbb8
nguyenhy <nguyenhy311@gmail.com>
2023-06-27 05:39:31 +0700

    style(header): add horizontal padding for mobile view

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,7 +12,7 @@ export default component$<IHeaderProps>(() => {
 
   return (
     <header class="sticky top-0 z-10 h-20 w-full bg-[#ffffff9e] py-2 overflow-hidden backdrop-blur-xl backdrop-saturate-[4.5]">
-      <div class="2xl:max-w-5xl container mx-auto flex flex-row justify-between items-center">
+      <div class="px-1 2xl:max-w-5xl container mx-auto flex flex-row justify-between items-center">
         <a class="no-underline" href="/">
           {/* h-20 ->  5rem: 80px */}
           {/* p-2 -> 0.5rem: 8px */}
