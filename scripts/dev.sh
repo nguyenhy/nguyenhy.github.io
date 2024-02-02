a34ec6f8f609ada3cd39c98847f800071d1aece2
nguyenhy <nguyenhy311@gmail.com>
2024-02-02 21:53:02 +0700

    feat(scripts): add scripts to get ip with run dev with `--mobile`

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -e
-./scripts/pre-build.sh
-npx vite --mode ssr
+# set -e
+# ./scripts/pre-build.sh
+if [ "$1" == "--mobile" ]; then
+  ip=$(ipconfig getifaddr $(route -n get default | grep 'interface:' | awk '{print $2}'))
+  npx vite --mode ssr --host $ip
+else
+  npx vite --mode ssr
+fi
