0013bbcd8a7343bcc69b2185da34d1b74b4ab18a
nguyenhy <nguyenhy311@gmail.com>
2024-02-16 18:19:14 +0700

    feat(script): uncomment `dev` script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# set -e
-# ./scripts/pre-build.sh
+set -e
+./scripts/pre-build.sh
 if [ "$1" == "--mobile" ]; then
   ip=$(ipconfig getifaddr $(route -n get default | grep 'interface:' | awk '{print $2}'))
   npx vite --mode ssr --host $ip
